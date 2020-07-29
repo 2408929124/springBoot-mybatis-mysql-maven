ade3163e3a19080d57afe1ee3359b05cdd949654
2408929124 <2408929124@qq.com>
2020-07-29 16:57:22 +0800

    上传表结构

---
+++ src/main/resources/public/表结构.sql
@@ -0,0 +1,75 @@
+/*
+SQLyog Ultimate v12.08 (64 bit)
+MySQL - 8.0.13 : Database - spring_boot
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`spring_boot` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+
+USE `spring_boot`;
+
+/*Table structure for table `tb_admin_user` */
+
+DROP TABLE IF EXISTS `tb_admin_user`;
+
+CREATE TABLE `tb_admin_user` (
+  `id` bigint(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `user_name` varchar(20) NOT NULL DEFAULT '' COMMENT '用户名',
+  `password_md5` varchar(50) NOT NULL DEFAULT '' COMMENT '密码',
+  `user_token` varchar(50) NOT NULL DEFAULT '' COMMENT 'token值',
+  `is_deleted` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否已删除 0未删除 1已删除',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '添加时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `tb_ssm_article` */
+
+DROP TABLE IF EXISTS `tb_ssm_article`;
+
+CREATE TABLE `tb_ssm_article` (
+  `id` int(4) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `article_title` varchar(200) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '' COMMENT '文章标题',
+  `article_content` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '文章内容',
+  `add_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '' COMMENT '添加人',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `is_deleted` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否已删除 0未删除 1已删除',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `tb_ssm_picture` */
+
+DROP TABLE IF EXISTS `tb_ssm_picture`;
+
+CREATE TABLE `tb_ssm_picture` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `path` varchar(200) DEFAULT NULL COMMENT '图片路径',
+  `remark` varchar(200) DEFAULT NULL COMMENT '备注',
+  `is_deleted` tinyint(200) DEFAULT '0' COMMENT '是否已删除 0未删除 1 已删除',
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '添加事件',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `tb_user` */
+
+DROP TABLE IF EXISTS `tb_user`;
+
+CREATE TABLE `tb_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `name` varchar(100) NOT NULL DEFAULT '' COMMENT '登录名',
+  `password` varchar(100) NOT NULL DEFAULT '' COMMENT '密码',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
